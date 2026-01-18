e3a9c6294f70699fc4d1efc3c5a8421f92286e56
myrsgsm <myrsgsm@gmail.com>
2026-01-19 02:34:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'royal-stock-v1';
+const urlsToCache = [
+  'StockView.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
